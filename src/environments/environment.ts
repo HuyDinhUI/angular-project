c48c90effefa740bf887537b867928ba7cbbaf9f
HuyDinhUI <146050567+HuyDinhUI@users.noreply.github.com>
2025-08-24 17:05:58 +0700

    change eviroment (test)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
   apiUrl: 'api',
-  ApiRoot: 'http://localhost:5267/api',
+  ApiRoot: 'http://localhost:3100/api',
   ApiJeeAccount: 'https://localhost:5001/api', //
 };
